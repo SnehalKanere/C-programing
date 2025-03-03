45e9811a0dde49730da555bdbe95ad7e68ce2afe
Snehal Kanere <kaneresnehal@gmail.com>
2025-03-03 14:50:10 +0530

    On the basis of percentage give the class like distinction,first class,second class,etc

---
+++ Program17.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void DisplayClass(float Marks)
+{
+    if((Marks>=0) && (Marks<35.00))
+    {
+        printf("You are fail..\n");
+    }
+    else if((Marks>=35.00) && (Marks<50.00))
+    {
+        printf("Your class is Pass class\n");
+    }
+  else if((Marks>=50.00) && (Marks<60.00))
+    {
+        printf("Your class is Second class\n");
+    }
+  else if((Marks>=60.00) && (Marks<75.00))
+    {
+        printf("Your class is First class\n");
+    }
+  else if((Marks>=75.00) && (Marks<=100.00))
+    {
+        printf("Your class is First class with Distinction\n");
+    }
+
+}
+
+
+int main()
+{
+    float Value1=0.0f;
+
+    printf("Enter your percentage:\n");
+    scanf("%f",&Value1);
+
+    DisplayClass(Value1);
+
+
+    return 0;
+}
